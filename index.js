2441c0fa2d1731e41c25f6bef5fafe471b18c7bd
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-08-19 14:15:52 -0300

    pequena correção

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ document.body.appendChild(renderer.domElement);
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 10);
 camera.position.z = 2;
 
-const scene = new THREE.Scene();
+const cena = new THREE.Scene();
 new OrbitControls(camera, renderer.domElement);
 
-const geometria = new THREE.IcosahedronGeometry(1, 12);
+const geometria = new THREE.IcosahedronGeometry(1, 14);
 const mat  = new THREE.MeshStandardMaterial({
     color: 0xffff00,
     // flatShading: true
 });
 
 const terra = new THREE.Mesh(geometria, mat);
-scene.add(terra);
+cena.add(terra);
 
 const luz = new THREE.HemisphereLight(0xFFFFFF, 3);
-scene.add(luz);
+cena.add(luz);
 
 function animate() {
   requestAnimationFrame(animate);
   terra.rotation.x += 0.001;
   terra.rotation.y += 0.002;
-  renderer.render(scene, camera);
+  renderer.render(cena, camera);
 }
 animate();

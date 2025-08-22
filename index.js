cf99800d17c2ac840fb66c896e82dad689830dac
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-08-22 14:02:43 -0300

    criando a mesh de luz

--- index.js
+++ index.js
@@ -18,23 +18,35 @@ cena.add(terraGrupo);
 new OrbitControls(camera, renderer.domElement);
 
 const loader = new THREE.TextureLoader();
-const geometria = new THREE.IcosahedronGeometry(1, 14);
+const geometria = new THREE.IcosahedronGeometry(1, 12);
 const mat  = new THREE.MeshStandardMaterial({
   map: loader.load('./texturas/00_earthmap1k.jpg'),
 });
 
-const terra = new THREE.Mesh(geometria, mat);
-terraGrupo.add(terra);
+const terraMesh = new THREE.Mesh(geometria, mat);
+terraGrupo.add(terraMesh);
 
 const estrelas = getConstelação({ numEstrelas: 2000 });
 cena.add(estrelas);
 
-const luz = new THREE.HemisphereLight(0xFFFFFF, 3);
-cena.add(luz);
+const luzMat = new THREE.MeshBasicMaterial({
+  map: loader.load('./texturas/03_earthlights1k.jpg'),
+  blending: THREE.AdditiveBlending,
+  transparent: true,
+  opacity: 0.5
+});
+
+const luzMesh = new THREE.Mesh(geometria, luzMat);
+terraGrupo.add(luzMesh);
+
+const luzSolar = new THREE.DirectionalLight(0xffffff);
+luzSolar.position.set(-2, 0.5, 1.5);
+cena.add(luzSolar);
 
 function animate() {
   requestAnimationFrame(animate);
-  terra.rotation.y += 0.002;
+  terraMesh.rotation.y += 0.002;
+  luzMesh.rotation.y += 0.002;
   renderer.render(cena, camera);
 }
 animate();
